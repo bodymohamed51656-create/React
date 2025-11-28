cfb62c6bac1b67578c8f7c1a933517ccad26389f
Abdullah Aboseif <bodymohamed51656@gmail.com>
2025-11-28 23:41:46 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 export default function App() {
   return (
     <>
-   <ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
+
       
      
 
